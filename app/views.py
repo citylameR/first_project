d73985a1f6979e94d3792892f950bf4c943c5649
David <dvdchernichenko@gmail.com>
2023-03-22 23:19:26 +0300

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+import datetime
+
+
+def time_view(request):
+    # обратите внимание – здесь HTML шаблона нет, 
+    # возвращается просто текст
+    current_time = datetime.datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+import os
+
+
+def workdir_view(request):
+    workdir = os.listdir(path='.')
+    msg = f'Текущая директория: {workdir}'
+    raise HttpResponse(msg)
